b6ab1fe776c6aa51c99fdc25fad5ad35e8e695f9
itaditya <adityaa803@gmail.com>
2016-04-04 19:20:54 +0530

    Frontend

---
+++ getzeal/js/script.js
@@ -0,0 +1,18 @@
+var greet=[ 
+	"You look \nAwesome !" , 
+	"This one's a \nKiller !" ,
+	"That's an\nAmazing picture !",
+	"You are looking\nGreat !"
+
+]
+console.log(greet);
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function displayMessage() {
+	var i=getRandomInt(0,greet.length-1);
+	var greeting=greet[i];
+	console.log(greet);
+	document.getElementById('greeting').innerHTML=greeting;
+	document.getElementById('supplement').innerHTML="Share your new Zealfie and make everyone envious";
+}
